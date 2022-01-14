7980376a32222b59dd190f224352a993981ea222
Alireza Esmikhani <world-window@Alirezas-MacBook-Air.local>
2022-01-14 09:00:07 +0330

    Improving code quality

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+"""The main function that converts your SEMRush API request to a DataFrame."""
+
 import requests
 import pandas as pd
+import config
 
 # The report type
-report_type = ''
+REPORT_TYPE = ''
 # An identification key assigned to a user after subscribing to SEMrush. The key is available on the Profile page
-key = ''
+KEY = config.API_KEY
 # A unique name of a website you’d like to investigate
-domain = ''
-# A regional database (one value from the list) / Base on your 'report_type' you need to pass the database argument
-# database = ''
+DOMAIN = ''
+# A regional database (one value from the list) / Base on your 'REPORT_TYPE' you need to pass the database argument
+# database = ''""
 
 
-def req_to_SEMRush_API():
+def req_to_semrush_api():
+    """Convert SEMRush API request to a Pandas DataFrame."""
     # You can modify your request options here
-    req = f"https://api.semrush.com/?type={report_type}&key={key}&domain={domain}&export_escape=1"
+    req = f"https://api.semrush.com/?type={REPORT_TYPE}&key={KEY}&domain={DOMAIN}&export_escape=1"
 
     res = requests.get(req).text
 
